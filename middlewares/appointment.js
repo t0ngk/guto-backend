49c07dc5ddc7c73659a5f03f79e29985d15cf601
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-05-21 21:31:54 +0700

    :bug: Perm

--- middlewares/appointment.js
+++ middlewares/appointment.js
@@ -16,14 +16,23 @@ const isOwner = async (req, res, next) => {
     });
   }
 
-  if (appointment.Pet.userId === req.user.id && req.user.role === "USER") {
-    req.appointment = appointment;
-    next();
-  } else {
-    res.status(401).json({
-      message: "You don't have permission to access this appointment"
+  // if (appointment.Pet.userId === req.user.id && req.user.role === "USER") {
+  //   req.appointment = appointment;
+  //   next();
+  // } else {
+  //   res.status(401).json({
+  //     message: "You don't have permission to access this appointment"
+  //   });
+  // }
+
+  if (appointment.Pet.userId !== req.user.id && req.user.role !== "ADMIN") {
+    return res.status(403).json({
+      error: "forbidden",
     });
   }
+
+  req.appointment = appointment;
+  next();
 }
 
 module.exports.isOwner = isOwner;
